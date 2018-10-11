9f3e15a418ed0a79e6bf0be4900cd81b2d8755f5
liaochenghao <1295316148@qq.com>
2018-10-11 18:04:25 +0800

    commit

--- record/models.py
+++ record/models.py
@@ -2,7 +2,15 @@ from django.db import models
 
 
 class User(models.Model):
-    open_id = models.CharField('open_id', max_length=64, primary_key=True)
+    OPTION_CHOICE = (
+        (0, '关注加拿大留学生问吧'),
+        (1, '关注北美留学生'),
+        (2, '全部关注'),
+        (3, '没关注'),
+    )
+    open_id = models.CharField('open_id', max_length=64)
+    union_id = models.CharField('union_id', max_length=64)
+    operation = models.IntegerField('操作类型', choices=OPTION_CHOICE)
     name = models.CharField('姓名', max_length=64, null=True)
     email = models.EmailField('邮箱', max_length=30, null=True)
     phone = models.CharField('联系手机', max_length=30, null=True)
